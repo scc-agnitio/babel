5ccd2628d892ce37f012e361e05c9df72f93244c
Julian C <lyhan_jr@hotmail.com>
2018-10-04 13:01:50 +0200

    Added data for languages en,es,de

--- database/data/commands.sh
+++ database/data/commands.sh
@@ -1,4 +1,4 @@
 # Convert iOS file to import (assuming same order/ammount of terms as in original english)
 
-cat -n ios_spanish.txt  | tr '\t' ',' | sed -e '/^$/d' -e 's/\ =\ /,/g' -e 's/;$//g' >> terms_translation_es.txt
+cat -n ios_spanish.txt  | tr '\t' ',' | sed -e '/^$/d' -e 's/\ =\ /,/g' -e 's/;$//g' -e 's/$/,"es"/g'>> terms_translation_es.txt
 
