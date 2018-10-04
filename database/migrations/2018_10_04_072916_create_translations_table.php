5d9ab77a83a0d90ecca6c2a6f6b125ac7179d248
Julian C <lyhan_jr@hotmail.com>
2018-10-04 09:43:29 +0200

    Added translations table

---
+++ database/migrations/2018_10_04_072916_create_translations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('term_id');
+            $table->string('language_code');
+            $table->text('text');
+            $table->timestamps();
+
+            $table->foreign('term_id')->references('id')->on('terms')->onDelete('cascade');
+            $table->foreign('language_code')->references('code')->on('languages')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('translations');
+    }
+}
